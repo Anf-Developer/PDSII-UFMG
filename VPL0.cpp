216c26ac580fe9baa056e8f9a512fddf130547e2
André Nunes Fernandes <andrenunes.ufmg@gmail.com>
2025-08-24 18:34:22 -0300

    Create VPL0.cpp

---
+++ VPL0.cpp
@@ -0,0 +1,13 @@
+// VPL 00: "Hello World"
+// Autor: André Nunes Fernandes
+// Matéria: Programação e Desenvolvimento de Software II
+// Curso: Engenharia de Controle e Automação
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+ cout << "Oi, Mundo!" << endl;
+ return 0;
+}
